b25351cf6ea618514c24d9c1cceb1390478539eb
Lawrence <e0176571@u.nus.edu>
2023-04-09 21:32:57 +0800

    increase route grade tags to 2 rows

--- src/components/Badge/RouteBadgeIterator.vue
+++ src/components/Badge/RouteBadgeIterator.vue
@@ -9,7 +9,7 @@ defineProps({
 <template>
   <template v-for="(gradeCount, i) in gradeCounts" :key="gradeCount.grade">
     <RouteBadge
-      v-if="i <= 5"
+      v-if="i <= 7"
       :grade-int="gradeCount.gradeInt"
       :grade-count="gradeCount.count"
       :grade-string="gradeCount.grade"
@@ -17,7 +17,7 @@ defineProps({
   </template>
   <span
     class="text-xs font-medium px-1.5 py-0.5 rounded bg-neutral-100"
-    v-if="gradeCounts && gradeCounts.length > 6"
-    >+ {{ gradeCounts.length - 6 }}</span
+    v-if="gradeCounts && gradeCounts.length > 8"
+    >+ {{ gradeCounts.length - 8 }}</span
   >
 </template>
